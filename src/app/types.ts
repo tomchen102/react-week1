55db97f9b0202ddfc38142b3b8e6c2bc0198d6b3
tom <tomchenforwork@gmail.com>
2025-01-13 00:57:48 +0800

    feat: 新增型別˙

---
+++ src/app/types.ts
@@ -0,0 +1,14 @@
+export interface ProductsDataProps {
+  category: string;
+  content: string;
+  description: string;
+  id: string;
+  is_enabled: number;
+  origin_price: number;
+  price: number;
+  title: string;
+  unit: string;
+  num: number;
+  imageUrl: string;
+  imagesUrl: string[];
+}
