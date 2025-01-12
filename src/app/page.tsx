4db00ae60a90fac6f63ff18ebc3600943af2e101
tom <tomchenforwork@gmail.com>
2025-01-13 00:58:07 +0800

    feat: 頁面和react code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,168 @@
+"use client";
+import { ProductsDataProps } from "./types";
+import { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
 import Image from "next/image";
+function App() {
+  const [tempProduct, setTempProduct] = useState<ProductsDataProps | null>(
+    null
+  );
+  const [productList, setProductList] = useState<ProductsDataProps[]>([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const result = await products();
+      setProductList(result);
+    };
+    fetchProducts();
+  }, []);
+  useEffect(() => {
+    const rootElement = document.getElementById("root");
+    if (rootElement) {
+      const root = createRoot(rootElement);
+      root.render(<App />);
+    }
+  }, []);
+  const products = async (): Promise<ProductsDataProps[]> => {
+    return [
+      {
+        category: "甜甜圈",
+        content: "尺寸：14x14cm",
+        description:
+          "濃郁的草莓風味，中心填入滑順不膩口的卡士達內餡，帶來滿滿幸福感！",
+        id: "-L9tH8jxVb2Ka_DYPwng",
+        is_enabled: 1,
+        origin_price: 150,
+        price: 99,
+        title: "草莓莓果夾心圈",
+        unit: "元",
+        num: 10,
+        imageUrl:
+          "https://images.unsplash.com/photo-1583182332473-b31ba08929c8",
+        imagesUrl: [
+          "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a",
+          "https://images.unsplash.com/photo-1559656914-a30970c1affd",
+        ],
+      },
+      {
+        category: "蛋糕",
+        content: "尺寸：6寸",
+        description:
+          "蜜蜂蜜蛋糕，夾層夾上酸酸甜甜的檸檬餡，清爽可口的滋味讓人口水直流！",
+        id: "-McJ-VvcwfN1_Ye_NtVA",
+        is_enabled: 1,
+        origin_price: 1000,
+        price: 900,
+        title: "蜂蜜檸檬蛋糕",
+        unit: "個",
+        num: 1,
+        imageUrl:
+          "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        imagesUrl: [
+          "https://images.unsplash.com/photo-1618888007540-2bdead974bbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80",
+        ],
+      },
+      {
+        category: "蛋糕",
+        content: "尺寸：6寸",
+        description: "法式煎薄餅加上濃郁可可醬，呈現經典的美味及口感。",
+        id: "-McJ-VyqaFlLzUMmpPpm",
+        is_enabled: 1,
+        origin_price: 700,
+        price: 600,
+        title: "暗黑千層",
+        unit: "個",
+        num: 15,
+        imageUrl:
+          "https://images.unsplash.com/photo-1505253149613-112d21d9f6a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+        imagesUrl: [
+          "https://images.unsplash.com/flagged/photo-1557234985-425e10c9d7f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxjYWtlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+          "https://images.unsplash.com/photo-1540337706094-da10342c93d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+        ],
+      },
+    ];
+  };
 
-export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <h2>產品列表</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>產品名稱</th>
+                <th>原價</th>
+                <th>售價</th>
+                <th>是否啟用</th>
+                <th>查看細節</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productList.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.title}</td>
+                  <td>{item.origin_price}</td>
+                  <td>{item.price}</td>
+                  <td>{item.is_enabled ? "啟用" : "未啟用"}</td>
+                  <td>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => setTempProduct(item)}
+                    >
+                      查看細節
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="col-md-6">
+          <h2>單一產品細節</h2>
+          {tempProduct ? (
+            <div className="card mb-3">
+              <Image
+                src={tempProduct.imageUrl}
+                className="card-img-top primary-image"
+                alt="主圖"
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {tempProduct.title}
+                  <span className="badge bg-primary ms-2">
+                    {tempProduct.category}
+                  </span>
+                </h5>
+                <p className="card-text">商品描述：{tempProduct.category}</p>
+                <p className="card-text">商品內容：{tempProduct.description}</p>
+                <div className="d-flex">
+                  <p className="card-text text-secondary">
+                    <del>{tempProduct.origin_price}</del>
+                  </p>
+                  元 / {tempProduct.price} 元
+                </div>
+                <h5 className="mt-3">更多圖片：</h5>
+                <div className="d-flex flex-wrap">
+                  {tempProduct.imagesUrl.map((url, index) => (
+                    <Image
+                      key={index}
+                      src={url}
+                      className="images"
+                      alt="更多圖片"
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p className="text-secondary">請選擇一個商品查看</p>
+          )}
+        </div>
+      </div>
     </div>
   );
+}
+
+export default function Home() {
+  return <div id="root"></div>;
 }
