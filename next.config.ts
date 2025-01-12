1b0312edea6ffec39be01505ab75d9e0f5609005
tom <tomchenforwork@gmail.com>
2025-01-13 01:23:22 +0800

    feat: 圖片可正常顯示

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
